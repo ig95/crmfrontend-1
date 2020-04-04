845397bae51d4bbe21d04802dfb6dc3ecf1c8405
cerebrium <nicholas.m.shankland@gmail.com>
2020-04-03 17:33:55 -0700

    form

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,17 @@ const App = () => {
   // const [ user, setUser] = useState(null);
 
   // dev mode
-  // useEffect( () => {
-  //   setUserName('Nicholas Shankland')
-  //   setUserEmail('nicholas.m.shankland@gmail.com')
-  //   setUserId('1923874-98y')
-  // },[])
+  useEffect( () => {
+    setUserName('Nicholas Shankland')
+    setUserEmail('nicholas.m.shankland@gmail.com')
+    setUserId('1923874-98y')
+  },[])
 
   // handles writting data to database and recieving google data
   const responseGoogle = (response) => {
-    setUserName(response.profileObj.givenName)
-    setUserId(response.profileObj.googleId)
-    setUserEmail(response.profileObj.email)
+    // setUserName(response.profileObj.givenName)
+    // setUserId(response.profileObj.googleId)
+    // setUserEmail(response.profileObj.email)
       // axios.post('/user/signup', {
       //   name: response.profileObj.name, 
       //   email: response.profileObj.email, 
--- src/components/DivSingleWeek.jsx
+++ src/components/DivSingleWeek.jsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const DivSingleWeek = (props) => {
-    const [ topRow, setTopRow ]= useState([])
-    const [ middleRow, setMiddleRow ]= useState([])
-    const [ bottomRow, setBottomRow ]= useState([])
+    const [ topRow, setTopRow ] = useState([])
+    const [ middleRow, setMiddleRow ] = useState([])
+    const [ bottomRow, setBottomRow ] = useState([])
 
     useEffect( () => {
         // top row mapped divs
@@ -23,6 +24,16 @@ const DivSingleWeek = (props) => {
             return thisWeekDivsArray
         }
 
+        // bottom row mapped divs
+        function bottomDivs () {
+            var lastWeekDivsArray = []
+            lastWeekDivsArray.push(<div className='cal_divs_single_first'><h4>Total for week:</h4></div>)
+            for (let i = 0; i < 7; i++) {
+                lastWeekDivsArray.push(<div key={i+50} className='cal_divs_single'><h5 className='inner_calander_text'>{Math.floor(Math.random() * 100)}</h5></div>)
+            }  
+            return lastWeekDivsArray
+        }
+
         // middle row mapped divs
         function middleRows () {
             var mappedProps = []
@@ -37,17 +48,17 @@ const DivSingleWeek = (props) => {
                 for (let i = 0; i < 7; i++) {
                     if (localAwesomeArray[i] !== -1) {
                         // logic for date booked or not
-                        mappedProps.push(<div key={Math.random()} className='cal_divs_single_booked'>
-                            <h5 className='inner_calander_text'>
-                                'BOOKED'
-                            </h5>
+                        mappedProps.push(<div key={Math.random()} className='cal_divs_single_booked' onClick={(e) => handleClick(e, checkForDate[i], ele)}>
+                                <h5 className='inner_calander_text'>
+                                    BOOKED
+                                </h5>
                         </div>)
                     } else {
                         // logic for date booked or not
-                        mappedProps.push(<div key={Math.random()} className='cal_divs_single_table'>
-                            <h5 className='inner_calander_text'>
-                                
-                            </h5>
+                        mappedProps.push(<div key={Math.random()} className='cal_divs_single_table' onClick={(e) => handleClick(e, checkForDate[i], ele)}>
+                                <h5 className='inner_calander_text'>
+                                    ---
+                                </h5>
                         </div>)
                     }
                 }  
@@ -56,15 +67,25 @@ const DivSingleWeek = (props) => {
             return mappedProps
         }
 
-        // bottom row mapped divs
-        function bottomDivs () {
-            var lastWeekDivsArray = []
-            lastWeekDivsArray.push(<div className='cal_divs_single_first'><h4>Total for week:</h4></div>)
-            for (let i = 0; i < 7; i++) {
-                lastWeekDivsArray.push(<div key={i+50} className='cal_divs_single'><h5 className='inner_calander_text'>{Math.floor(Math.random() * 100)}</h5></div>)
-            }  
-            return lastWeekDivsArray
+        const handleSubmitButton = () => {
+            setMiddleRow(middleRows())
         }
+
+        const makeForm = (dateSelection, nameSelection) => {
+            return (
+                <div className='inner_calender_form'>
+                    <h3>{nameSelection}</h3>
+                    <h3>{dateSelection}</h3>
+                    <button onClick={handleSubmitButton}>Add Work</button>
+                </div>
+            )
+        }
+
+        const handleClick = (e, weekDaySelected, theName) => {
+            setMiddleRow(makeForm(weekDaySelected, theName))
+
+        }
+
 
         setTopRow(thisWeekDivs())
         setMiddleRow(middleRows())
