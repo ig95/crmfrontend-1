93f372436085bee0d7f9bfb64e339675437b1382
cerebrium <nicholas.m.shankland@gmail.com>
2020-04-29 17:19:13 -0700

    fixed all name searches

--- src/pages/VehicleChecklist.jsx
+++ src/pages/VehicleChecklist.jsx
@@ -6,6 +6,9 @@ const VehicleChecklist = (props) => {
     const [ selectedDriver, setSelectedDriver ] = useState(null)
     const [ drivers, setDrivers ] = useState(null)
     const [ driverSearchArray, setDriverSearchArray ] = useState([])
+    const [ dataset, setDataset ] = useState(null)
+    const [ makeSearchBarVisible, setMakeSearchBarVisible ] = useState('dashboard_form_divs_name_bar_none')
+    const [ nameValue, setNameValue ] = useState('')
 
     // make this like the daily operations page. show vehicle recommendation in entry column
     // location rota system shows not exceeding 7 days for same person
@@ -30,6 +33,9 @@ const VehicleChecklist = (props) => {
 
         getData('https://pythonicbackend.herokuapp.com/drivers/').then( (response) => {
             setDrivers(response.results)
+            getData('https://pythonicbackend.herokuapp.com/data/').then( (response) => {
+                setDataset(response.data.drivers)
+            })
         })
     }, [])
     
@@ -42,32 +48,58 @@ const VehicleChecklist = (props) => {
         )
     }
 
+     // select name
+     const handleNameClick = (e, theName) => {
+        makeSearchUnderBar('', 10)
+        setNameValue(theName)
+        dataset.forEach( (ele, id) => {
+            if (ele.name === theName) {
+                setSelectedDriver(ele)
+            }
+        })
+    }
+
+    // search bar 
+    const makeSearchUnderBar = (theValue, theLength) => {
+        if (theValue !== '' && theLength <= 3) {
+            setMakeSearchBarVisible('dashboard_form_divs_name_bar_docs')
+        } else {
+            setMakeSearchBarVisible('dashboard_form_divs_name_bar_none')
+        }
+    }
+
     // search bar function
     const handleChange = (e) => {
-        let localArray = []
-        drivers.forEach( (ele, id) => {
-            if (ele.name.includes(e.target.value) && e.target.value !== '' && e.target.value.length < 3) {
-                localArray.push(
-                    <h4 key={id}>{ele.name}</h4>
-                )
-            }
-            if (e.target.value === ele.name) {
-                setSelectedDriver(ele)
-            }    
-        })
-        setDriverSearchArray(localArray)
+        if (dataset) {
+            setNameValue(e.target.value)
+            makeSearchUnderBar(e.target.value, nameValue.length)
+            let localArray = []
+            dataset.forEach( (ele, id) => {
+                if (ele.name.includes(e.target.value) && e.target.value !== '' && e.target.value.length < 4) {
+                    localArray.push(
+                        <h4 className='name_suggestions' onClick={(e, theName) => handleNameClick(e, `${ele.name}`)}>{ele.name}</h4>
+                    )
+                }
+            })
+            setDriverSearchArray(localArray)
+        }
     }
+
 
     return (
         <div className='home_content'>
             <NavigationBar title='Vehicle Checklist'/>
             <div className='main_content_driver_documents'>
                 <div className='documents_search_bar'>
-                    <label >Find Driver </label><br />
-                        <input className='search_bar' type="text" name='searchBar' onChange={handleChange} />
+                    <div className='invoice_form_divs_name'>
                         <div>
+                            <label className='dashboard_labels'>Find Driver </label>
+                        </div>
+                            <input className='search_bar' type="text" name='Name' value={nameValue} onChange={handleChange} autoComplete='off'/>
+                        <div className={`${makeSearchBarVisible}`}>
                             {driverSearchArray}
                         </div>
+                    </div>
                 </div>
                 {content}
             </div>
